f4d3d034fae08d98b6ab74208d26bf42ab79685a
PRANJAY GULERIA <pranjayguleria@gmail.com>
2023-04-07 18:29:29 +0530

    Updates

---
+++ precache-manifest.d9d21ccd0cbfb9e898c0a87950a73ad3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfbddc11ee55ef6952e1",
+    "url": "/Tution_Verse/static/css/main.80807ef2.chunk.css"
+  },
+  {
+    "revision": "dfbddc11ee55ef6952e1",
+    "url": "/Tution_Verse/static/js/main.dfbddc11.chunk.js"
+  },
+  {
+    "revision": "4732dc574e7f56a2904c",
+    "url": "/Tution_Verse/static/js/1.4732dc57.chunk.js"
+  },
+  {
+    "revision": "9162d90cacba4cc95ccc",
+    "url": "/Tution_Verse/static/js/runtime~main.9162d90c.js"
+  },
+  {
+    "revision": "0d999b66ab0224d5b597350fbd3236ec",
+    "url": "/Tution_Verse/static/media/Logo.0d999b66.svg"
+  },
+  {
+    "revision": "0744e3c1b8af6599ea6cef1bb907f17a",
+    "url": "/Tution_Verse/static/media/home-banner-background.0744e3c1.png"
+  },
+  {
+    "revision": "c01253e488e5e4e773f87c363cf8eff5",
+    "url": "/Tution_Verse/static/media/home-banner-image.c01253e4.gif"
+  },
+  {
+    "revision": "e52235c48475287cb685c8da59c0b88b",
+    "url": "/Tution_Verse/static/media/about-background-image.e52235c4.png"
+  },
+  {
+    "revision": "b9c0b14a832a107d3cd6738c6e77e3c5",
+    "url": "/Tution_Verse/static/media/tester.b9c0b14a.jpeg"
+  },
+  {
+    "revision": "1e244731fe19d75399ef40389714e3a0",
+    "url": "/Tution_Verse/index.html"
+  }
+];
